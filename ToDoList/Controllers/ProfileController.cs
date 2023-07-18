9a4dffd442798e033cdc26c4932fdde77bc99270
Lemone28 <lemone681@gmail.com>
2023-07-18 19:27:06 +0300

    Implemented authentication

--- ToDoList/Controllers/ProfileController.cs
+++ ToDoList/Controllers/ProfileController.cs
@@ -1,30 +1,69 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using ToDoList.DB;
 using ToDoList.Models.Profile;
 using ToDoList.Shared.Entity;
+using ToDoList.Shared.Helpers;
 
 namespace ToDoList.Controllers
 {
-	public class ProfileController : Controller
-	{
-		[Authorize]
-		public IActionResult Index()
-		{
-			ClaimsPrincipal claims = HttpContext.User;
-			return View(new IndexModel
-			{
-				User = new UserEntity
-				{
-					Id = int.Parse(claims.FindFirstValue("Id")),
-					Login = claims.FindFirstValue("Login"),
-					Password = claims.FindFirstValue("Password"),
-					Email = claims.FindFirstValue("Email"),
-					Phone = claims.FindFirstValue("Phone"),
-					FirstName = claims.FindFirstValue("FirstName"),
-					LastName = claims.FindFirstValue("LastName")
-				}
-			});
-		}
-	}
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+        private readonly ApplicationContext _db;
+
+        public ProfileController(ILogger<ProfileController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            _db = context;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            ViewBag.IsAuthenticated = User.Identity.IsAuthenticated;
+        }
+
+        public IActionResult Index()
+        {
+            ClaimsPrincipal claims = HttpContext.User;
+            UserEntity user = new UserEntity
+            {
+                Id = int.Parse(claims.FindFirstValue("Id")),
+                Login = claims.FindFirstValue("Login"),
+                Password = claims.FindFirstValue("Password"),
+                Email = claims.FindFirstValue("Email"),
+                Phone = claims.FindFirstValue("Phone"),
+                FirstName = claims.FindFirstValue("FirstName"),
+                LastName = claims.FindFirstValue("LastName")
+            };
+            return View(new IndexModel { User = user });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEntity user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            try
+            {
+                _db.Users.Update(user);
+                await _db.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while updating user in the database.");
+                return StatusCode(500);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
 }
---
+++ ToDoList/Models/Account/SignUpModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ToDoList.Shared.Entity;
+
+namespace ToDoList.Models.Account
+{
+	public class SignUpModel
+	{
+		public UserEntity User { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
