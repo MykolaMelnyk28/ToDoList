cda97d8426e155161c4682b7f88d11d3f16412b5
Lemone28 <lemone681@gmail.com>
2023-07-21 17:26:44 +0300

    Update the data validation

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using ToDoList.Shared.Entity;
 using ToDoList.DB;
 using Microsoft.AspNetCore.Mvc.Filters;
 using ToDoList.Shared.Helpers;
+using ToDoList.Models.Account;
 
 namespace ToDoList.Controllers
 {
@@ -32,22 +33,36 @@ namespace ToDoList.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SignUp(UserEntity user, string confirmPassword)
+        public async Task<IActionResult> SignUp(SignUpModel model)
         {
-            if(!UserEntityHelper.IsValid(user))
+            if (model == null || model.User == null || model.ConfirmPassword == null)
             {
-                return Unauthorized();
+                return BadRequest();
             }
 
-            if(user.Password != confirmPassword)
+            if(model.User.Password != model.ConfirmPassword)
             {
-                ModelState.AddModelError("ConfirmPassword", "Паролі не співпадають");
-                return View();
+                ModelState.AddModelError("ConfirmPassword", "Паролі не співпадають.");
+            }
+
+            if(UserExists(model.User.Login))
+            {
+                ModelState.AddModelError("User.Login", "Користувач з таким логіном вже існує.");
+            }
+
+            if(EmailExists(model.User.Email))
+            {
+                ModelState.AddModelError("User.Email", "Користувач з такою електронною адресою вже існує.");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View(model);
             }
 
             try
             {
-                await _db.Users.AddAsync(user);
+                await _db.Users.AddAsync(model.User);
                 await _db.SaveChangesAsync();
             }
             catch(Exception ex)
@@ -56,32 +71,37 @@ namespace ToDoList.Controllers
                 return StatusCode(500);
             }
 
-            await _SignIn(user);
+            await SignInUser(model.User);
 
             return RedirectToAction("Index", "Home");
         }
 
         public IActionResult SignIn()
         {
-            return View();
+            return View(new SignInModel { User = new UserEntity { Login = "", Email = "", Password = ""} });
         }
 
         [HttpPost]
-        public IActionResult SignIn(UserEntity user)
+        public IActionResult SignIn(SignInModel model)
         {
-            if(!UserEntityHelper.IsValid(user))
+            if(model == null || model.User == null)
             {
                 return BadRequest();
             }
 
-            UserEntity foundUser = _db.Users.FirstOrDefault(x => x.Login == user.Login);
-            if(foundUser == null || foundUser.Password != user.Password)
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            UserEntity foundUser = _db.Users.FirstOrDefault(x => x.Login == model.User.Login);
+            if(foundUser == null || foundUser.Password != model.User.Password)
             {
                 ModelState.AddModelError(string.Empty, "Невірний логін або пароль");
                 return View();
             }
 
-            _SignIn(foundUser);
+            SignInUser(foundUser);
 
             return RedirectToAction("Index", "Home");
         }
@@ -92,20 +112,32 @@ namespace ToDoList.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        private async Task _SignIn(UserEntity user)
+        [NonAction]
+        private bool UserExists(string login)
+        {
+            return _db.Users.Count(x => x.Login == login) > 0;
+        }
+
+        [NonAction]
+        private bool EmailExists(string email)
+        {
+            return _db.Users.Count(x => x.Email == email) > 0;
+        }
+
+        [NonAction]
+        private async Task SignInUser(UserEntity user)
         {
             List<Claim> claims = new List<Claim>
             {
                 new Claim("Id", user.Id.ToString()),
                 new Claim("Login", user.Login),
-                new Claim("Password", user.Password),
                 new Claim("Email", user.Email),
                 new Claim("Phone", user.Phone),
                 new Claim("FirstName", user.FirstName),
                 new Claim("LastName", user.LastName)
             };
 
-            ClaimsIdentity identity = new ClaimsIdentity(claims, "ApplicationCookie");
+            ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
             ClaimsPrincipal principal = new ClaimsPrincipal(identity);
 
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
